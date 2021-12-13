9e4b1958534090e0c7db446157e523f0edae6c03
joonacode <joonacode@gmail.com>
2021-12-13 13:14:13 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ['avatars.dicebear.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
